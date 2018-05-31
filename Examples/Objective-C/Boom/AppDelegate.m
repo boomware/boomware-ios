65c7081ed708751f1765e259ca17560d3dfa1b86
Serj <serji.popov@gmail.com>
2018-05-31 19:05:20 +0300

    Added code sample.

--- Examples/Objective-C/Boom/AppDelegate.m
+++ Examples/Objective-C/Boom/AppDelegate.m
@@ -18,41 +18,25 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    [BWBoomware setKey:@"key:private:private"];
-    [BWBoomware verifyNumber:@"number" method:BWBoomwareMethodSMS completion:^(NSError * _Nullable error) {
-        NSLog(@"errror %@", error);
-    }];
-    NSLog(@"Launched");
-
+    // Init Boomware SDK
+    [BWBoomware setKey:@"youkey:youkey:youkey"];
     return YES;
 }
 
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    // Set device token
+    [BWBoomware setDeviceToken:deviceToken];
 }
 
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    // Check that the notification send by boomware
+    if ([BWBoomware isBoomwarePushNotification:userInfo]) {
+        // Handle it
+        [BWBoomware handlePushNotification:userInfo];
+    }
+
 }
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 
 @end
