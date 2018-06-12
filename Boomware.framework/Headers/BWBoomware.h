7e05b03b01eda50d85a23730302a54d82b9c5ebe
Serj <serji.popov@gmail.com>
2018-06-12 13:40:33 +0300

    Update license and version

--- Boomware.framework/Headers/BWBoomware.h
+++ Boomware.framework/Headers/BWBoomware.h
@@ -3,8 +3,24 @@
 //  Boomware
 //
 //  Created by Sergey P on 15.08.17.
-//  Copyright © 2017 boomware.com. All rights reserved.
+//  Copyright © 2018 Boomware (http://boomware.com/). All rights reserved.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software to use and to permit persons to whom the Software is furnished
+//  to do so in connection with the web services and APIs provided by Boomware,
+//  subject to the conditions described on the Boomware website and hereby:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
 
